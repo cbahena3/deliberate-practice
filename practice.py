306924672ed332276f82c789569c57fbc28ee806
Cristian B <cris.bahena77@gmail.com>
2023-10-30 22:37:55 -0500

    one through 10

--- practice.py
+++ practice.py
@@ -5,32 +5,47 @@ print(Robert + 10)
 
 #2. Write a program that uses a variable to store a word
 #then prints that word with all capital letters.
-
+thisUpper = 'this needs to be uppercase'
+print(thisUpper.upper())
 
 #3. Write a program that uses variables to store two numbers
 #then prints the numbers added together.
-
+oneTwo = 12
+TwoOne = 21
+print(oneTwo + TwoOne)
 
 #4. Write a program that uses a variable to store a word
 #then prints that word in reverse order.
+backwards = 'desserts'
+print(backwards[::-1])
 
 #5. Write a program that uses a variable to store a number
 #then prints the number times 10.
+multipied = 10
+print(multipied * 10)
 
 #6. Write a program that uses variables to store two words
 #then prints both words on the same line in all capital letters.
-
+first= 'happy'
+second= 'feet'
+print(f'{first.upper()} {second.upper()}')
 
 #7. Write a program that uses a variable to store a word
 #then prints the number of letters in the word.
+name= 'Cristian'
+print(len(name))
 
 #8. Write a program that uses a variable to store a number
 #with decimals, then prints the number as an integer.
-
+floaty= 1.32
+print(int(floaty))
 
 #9. Write a program that uses a variable to store two numbers
 #then prints the two numbers multiplied together.
-
-
+age= 22
+year= 2023
+print(age * year)
 #10. Write a program that uses a variable to store a word
 #then prints the word with all lowercase letters.
+myName= 'CRISTIAN BAHENA'
+print(myName.lower())
