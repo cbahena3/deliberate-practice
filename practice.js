754af94be58e64d5a8d590a0be15ac29d74965fb
Cristian B <cris.bahena77@gmail.com>
2023-10-31 10:40:06 -0500

    one through ten

--- practice.js
+++ practice.js
@@ -1,36 +1,51 @@
 /*1. Write a program that uses a variable to store a number,
 then prints that number plus 10.*/
-Rob = 2000;
+let Rob = 2000;
 console.log(Rob + 100);
 
 /*2. Write a program that uses a variable to store a word
 then prints that word with all capital letters.*/
-
+let myName = "Cristian";
+console.log(myName.toUpperCase());
 
 /* 3. Write a program that uses variables to store two numbers
 then prints the numbers added together. */
-
+let thisNum = 10;
+let thatNum = 12;
+console.log(thisNum + thatNum);
 
 /* 4. Write a program that uses a variable to store a word
 then prints that word in reverse order. */
-
+pizzaOne= 'Pizza'
+console.log(pizzaOne.split('').reverse().join(''))
 /* 5. Write a program that uses a variable to store a number
 then prints the number times 10. */
+oneHund= 100
+console.log(oneHund * 10)
 
 /* 6. Write a program that uses variables to store two words
 then prints both words on the same line in all capital letters. */
-
+state= 'illinois'
+city= 'chicago'
+console.log(state.toUpperCase() +" "+ city.toUpperCase())
 
 /* 7. Write a program that uses a variable to store a word
 then prints the number of letters in the word. */
+theWorld= 'Earth'
+console.log(theWorld.length)
 
 /* 8. Write a program that uses a variable to store a number
 with decimals, then prints the number as an integer. */
-
+thisDec= 1.234
+console.log(parseInt(thisDec))
 
 /* 9. Write a program that uses a variable to store two numbers
 then prints the two numbers multiplied together. */
-
+unoDos= 12
+DosTres= 23
+console.log(unoDos*DosTres)
 
 /* 10. Write a program that uses a variable to store a word
 then prints the word with all lowercase letters. */
+allCap = 'ALL CAP LETTERS'
+console.log(allCap.toLowerCase())
