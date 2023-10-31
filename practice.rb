2b24561dcfd068d6ebfabd45362627fec8d782e8
Cristian B <cris.bahena77@gmail.com>
2023-10-30 22:16:00 -0500

    one through ten

--- practice.rb
+++ practice.rb
@@ -2,3 +2,53 @@
 #then prints that number plus 10.
 bob = 215
 puts bob + 10
+
+
+#2. Write a program that uses a variable to store a word
+#then prints that word with all capital letters.
+wordyWord = 'ThisIsNotActuallyAwordJustAsentence'
+puts wordyWord.upcase 
+
+#3. Write a program that uses variables to store two numbers
+#then prints the numbers added together.
+numOne = 10
+numTwo = 20
+puts numOne + numTwo
+
+#4. Write a program that uses a variable to store a word
+#then prints that word in reverse order.
+theReverse = 'Akor'
+puts theReverse.reverse
+
+#5. Write a program that uses a variable to store a number
+#then prints the number times 10.
+numRando = 11
+puts numRando * 10
+
+#6. Write a program that uses variables to store two words
+#then prints both words on the same line in all capital letters.
+firstName = 'Cristian'
+lastName = 'Bahena'
+puts "#{firstName.upcase} #{lastName.upcase}"
+
+#7. Write a program that uses a variable to store a word
+#then prints the number of letters in the word.
+mrKrabs= 'Mr.Krabs'
+puts mrKrabs.length
+
+#8. Write a program that uses a variable to store a number
+#with decimals, then prints the number as an integer.
+deciamlNum = 1.3934920
+puts deciamlNum.to_i
+
+#9. Write a program that uses a variable to store two numbers
+#then prints the two numbers multiplied together.
+twoTwo= 22
+oneZero= 10
+puts twoTwo * oneZero
+
+#10. Write a program that uses a variable to store a word
+#then prints the word with all lowercase letters.
+
+smallWords= 'AAAAAAAHHH THIS IS UPPERCASE'
+puts smallWords.downcase
