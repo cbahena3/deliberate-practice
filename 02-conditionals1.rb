c96c9f2dc653686c879305267932384a573c041a
Cristian B <cris.bahena77@gmail.com>
2023-10-31 22:09:40 -0500

    02 conditionals1

--- 02-conditionals1.rb
+++ 02-conditionals1.rb
@@ -86,27 +86,15 @@ else:
 # 9. Use variables to store two numbers
 # then write a condition that prints 1 if the first number is less than zero 
 # and the second number is greater than 0, and prints 0 otherwise.
-num11 = -1
+num11 = 58
 num12 = 78943
-if num11 < 0 and num12 > 0:
-    print(1)
-else:
-    print(0)
+if num11 < 0 and num12
 
 
 # 10. Use a variable to store a number
 # then write a condition that prints 5 if the number is greater than 80
 # prints 4 if the number is greater than 60, prints 3 if the number is greater than 40
 # prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
-numLucky = 777
-if numLucky > 80:
-    print(5)
-elif numLucky > 60:
-    print(4)
-elif numLucky > 40:
-    print(3)
-elif numLucky > 20:
-    print(2)
-else:
-    print(1)
+
+
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------
