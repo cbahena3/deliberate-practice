691004e71507a20a5611021b81df7a679c8f919e
Cristian B <cris.bahena77@gmail.com>
2023-12-04 22:14:06 -0600

    created practice

---
+++ 05-custMthClass2.py
@@ -0,0 +1,37 @@
+#1 Write a Song class with attributes for name, artist, and duration.
+
+
+
+#2 Write a Rectangle class with attributes for width and height.
+
+
+
+#3 Write a Person class with attributes for name and age.
+
+
+
+#4 Write a Location class with attributes for latitude and longitude.
+
+
+
+#5 Write an Account class with attributes for name and balance.
+
+
+
+#6 Write a Movie class with attributes for title, director, and year.
+
+
+
+#7 Write a Car class with attributes for make, model, year, and color.
+
+
+
+#8 Write a Point class with attributes for x, y, and z coordinates.
+
+
+
+#9 Write a Book class with attributes for title, author, and year.
+
+
+
+#10 Write a Plant class with attributes for name, size, and price.
