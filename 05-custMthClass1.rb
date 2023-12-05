d574a89f60d0b992f6ed4d6e633e6a8656a70625
Cristian B <cris.bahena77@gmail.com>
2023-12-04 20:16:23 -0600

    added 3-10

--- 05-custMthClass1.rb
+++ 05-custMthClass1.rb
@@ -21,28 +21,52 @@ puts subbing(1, 3)
 
 # 4) Write a method that takes in a number and returns the number times itself.
 # Then run the method and print the result.
-
+def doubled(num)
+  return num * num
+end
+pp doubled(10)
 
 # 5) Write a method that takes in a string and returns the first letter of the string.
 # Then run the method and print the result.
-
+def firstLetter(string)
+  return string[0]
+end
+pp firstLetter("Apple")
 
 # 6) Write a method that takes in three strings and
 # returns a string that combines all three strings with spaces in between.
 # Then run the method and print the result.
-
+def interpolating(string1, string2, string3)
+  return "#{string1} #{string2} #{string3}"
+end
+pp interpolating("Hello", "My", "World")
 
 # 7) Write a method that takes in a number and returns the number as a string.
 # Then run the method and print the result.
-
+def numToStri(number)
+  return number.to_s
+end
+pp numToStri(10)
 
 # 8) Write a method that takes in a string and returns the string repeated 5 times.
 # Then run the method and print the result.
-
+def repeat(word)
+  return word*5
+end
+pp repeat("Hi")
 
 # 9) Write a method that takes in 3 numbers and returns the average (the sum divided by 3.0).
 # Then run the method and print the result.
-
+def avg(num1, num2, num3)
+  sum = num1 + num2 + num3
+  average = sum.to_f / 3.0
+  return average
+end
+pp avg(1, 2, 8)
 
 # 10) Write a method that takes in a number and returns the number times 10 plus 30.
 # Then run the method and print the result.
+def large(num)
+  return (num * 10) + 30
+end
+pp large(10)
