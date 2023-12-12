b6de63105ff1ebb3782c2b9333b53fd2e1ac090a
Cristian B <cris.bahena77@gmail.com>
2023-12-11 19:48:18 -0600

    all answered

--- 02-conditionals3.rb
+++ 02-conditionals3.rb
@@ -3,29 +3,91 @@
 # If the age is 12 years old or younger, the ticket price is $5.
 # If the age is between 13 and 59 years old and the movie is before 6 PM, the ticket price is $7. After 6 PM, the ticket price is $10.
 # If the customer is 60 years old or older, the ticket price is $7.
+age = 65
+movieTime = 19
 
-
+if age <= 12
+  pp "Your ticket is $5.00"
+elsif age >= 13 && age <= 59
+  if movieTime < 18
+    pp "Your ticket is $7.00"
+  else
+    pp "Your ticket is $10.00"
+  end
+elsif age >= 60
+  pp "Your ticket is $7.00"
+end
 
 # 2 Write a program to store the type of book (regular, reference, or special collection) and the number of days its overdue. Then calculate the fine amount based on the following conditions:
 
-# If the book is a regular book and overdue by up to 7 days, the fine is $1 per day.
-# If the book is a regular book and overdue by more than 7 days, the fine is $2 per day.
-# If the book is a reference book, there is no fine, regardless of the number of days overdue.
+#   If the book is a regular book and overdue by up to 7 days, the fine is $1 per day.
+#   If the book is a regular book and overdue by more than 7 days, the fine is $2 per day.
+#   If the book is a reference book, there is no fine, regardless of the number of days overdue.
 # If the book is a special collection book, the fine is $5 per day, regardless of the number of days overdue.
-
+bookType = "regular"
+daysOverdue = 10
+if bookType == "regular"
+  if daysOverdue <= 7
+    pp "Your fine is $#{1 * daysOverdue}"
+  else daysOverdue > 7
+    pp "Your fine is $#{2 * daysOverdue}"
+  end
+elsif bookType == "reference"
+  pp "You have no fine"
+elsif bookType == "special collection"
+  pp "Your fine is $#{5 * daysOverdue}"
+end
 
 # 3 Write a program that stores a person's order value and membership level (regular or premium). Then calculate a discount amount based on the following conditions:
 
 # If the total order value is less than $50, there is no discount.
 # If the total order value is between $50 and $100, the discount is 5% for regular customers and 10% for premium customers.
 # If the total order value is greater than $100, the discount is 10% for regular customers and 15% for premium customers.
+orderCost = 40
+membership = "premium"
 
+if orderCost < 50
+  pp "No discount for you"
+elsif orderCost >= 50 && orderCost <= 100
+  if membership == "regular"
+    pp "You get a 5% discount"
+  elsif membership == "premium"
+    pp "You get a 10% discount"
+  end
+elsif orderCost > 100
+  if membership == "regular"
+    pp "You get a 10% discount"
+  elsif membership == "premium"
+    pp "You get a 15% discount"
+  end
+end
 
 # 4 Write a Ruby program that stores the weight of a package and the destination (domestic or international). Then calculate the shipping fee based on the following conditions:
 
-# If the destination is domestic:
-# If the weight is less than or equal to 1 kg, the shipping fee is $5.
-# If the weight is greater than 1 kg, the shipping fee is $10.
+#   If the destination is domestic:
+#   If the weight is less than or equal to 1 kg, the shipping fee is $5.
+#   If the weight is greater than 1 kg, the shipping fee is $10.
 # If the destination is an international shipment:
 # If the weight is less than or equal to 1 kg, the shipping fee is $15.
 # If the weight is greater than 1 kg, the shipping fee is $25.
+
+weightKg = 10
+destination = "international"
+
+if destination == "domestic"
+  if weightKg <= 1
+    fee = 5.00
+    pp "Shipping fee: $#{fee}"
+  elsif weightKg > 1
+    fee = 10.00
+    pp "Shipping fee: $#{fee}"
+  end
+elsif destination == "international"
+  if weightKg <= 1
+    fee = 15.00
+    pp "Shipping fee: $#{fee}"
+  elsif weightKg > 1
+    fee = 25.00
+    pp "Shipping fee: $#{fee}"
+  end
+end
