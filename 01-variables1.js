86b82c4663be8e529f736faa50b7e1b94a5cd0d2
Cristian B <cris.bahena77@gmail.com>
2023-12-13 12:56:28 -0600

    fixed errors

--- 01-variables1.js
+++ 01-variables1.js
@@ -18,39 +18,39 @@ console.log(thisNum + thatNum);
 
 /* 4. Write a program that uses a variable to store a word
 then prints that word in reverse order. */
-pizzaOne= 'Pizza'
-console.log(pizzaOne.split('').reverse().join(''))
+let pizzaOne= 'Pizza';
+console.log(pizzaOne.split('').reverse().join(''));
 /* 5. Write a program that uses a variable to store a number
 then prints the number times 10. */
-oneHund= 100
-console.log(oneHund * 10)
+let oneHund= 100;
+console.log(oneHund * 10);
 
 /* 6. Write a program that uses variables to store two words
 then prints both words on the same line in all capital letters. */
-state= 'illinois'
-city= 'chicago'
-console.log(state.toUpperCase() +" "+ city.toUpperCase())
+let state= 'illinois';
+let city= 'chicago';
+console.log(state.toUpperCase() +" "+ city.toUpperCase());
 
 /* 7. Write a program that uses a variable to store a word
 then prints the number of letters in the word. */
-theWorld= 'Earth'
-console.log(theWorld.length)
+let theWorld= 'Earth';
+console.log(theWorld.length);
 
 /* 8. Write a program that uses a variable to store a number
 with decimals, then prints the number as an integer. */
-thisDec= 1.234
-console.log(parseInt(thisDec))
+let thisDec= 1.234;
+console.log(parseInt(thisDec));
 
 /* 9. Write a program that uses a variable to store two numbers
 then prints the two numbers multiplied together. */
-unoDos= 12
-DosTres= 23
-console.log(unoDos*DosTres)
+let unoDos= 12;
+let DosTres= 23;
+console.log(unoDos*DosTres);
 
 /* 10. Write a program that uses a variable to store a word
 then prints the word with all lowercase letters. */
-allCap = 'ALL CAP LETTERS'
-console.log(allCap.toLowerCase())
+let allCap = 'ALL CAP LETTERS';
+console.log(allCap.toLowerCase());
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------
 
