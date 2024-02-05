b9b55d264065ea77596a55b8d6b6e7b0533f7b00
Cristian B <cris.bahena77@gmail.com>
2024-02-05 10:56:14 -0600

    added answers for 1-7

--- 01-variables2.js
+++ 01-variables2.js
@@ -1,23 +1,48 @@
 // 1 Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+let firstName = 'joe';
+let lastName = 'hammy';
 
+console.log(firstName + " " + lastName)
 
 // 2 Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the # {} operator).
+let firstName2 = 'joeee';
+let lastName2 = 'hammyyyyy';
 
+console.log(`${firstName2} ${lastName2}`)
 
 // 3 Write a program that asks the user to input a word. If the word is "marco", print "polo".
-
+// let word = prompt("Enter a word:");
+// if (word === "marco") {
+//   console.log(`Hello ${word}`)
+//   console.log("polo");
+// }
 
 // 4 Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+let color = "blue";
+let color2 = "red";
+let color3 = "green";
 
+console.log(color + " "+ color2 + " "+ color3)
 
 // 5 Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the # {} operator).
+let colors = "blue";
+let colors2 = "red";
+let colors3 = "green";
 
+console.log(`${colors} ${colors2} ${colors3}`)
 
 // 6 Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+// let question = prompt("Enter a name:")
 
+// if(question.toLowerCase !== "santa"){
+//   console.log("You're not santa!!!!")
+// }
 
 // 7 Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+let bookName = "Python lesson";
+let author = 'Myself';
 
+console.log(bookName + " " + author)
 
 // 8 Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the # {} operator).
 
--- random.rb
+++
@@ -1,9 +0,0 @@
-words = ["banana", "jon", "apple"]
-letters = []
-a_count = 0
-
-words.each do |word|
-	word.split(",")
-end
-
-pp words
