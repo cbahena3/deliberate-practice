ef293b51035a3769be0e0c1bf5a60156142dadfd
Cristian B <cris.bahena77@gmail.com>
2024-03-18 09:57:50 -0500

    added 7-10 answers

--- 03-loops4.js
+++ 03-loops4.js
@@ -44,12 +44,32 @@ console.log(lowestPrice);
 
 // 7 Start with an array of numbers and compute product of all the numbers.
 //  For example, [5, 10, 8, 3] becomes 1200.
+let allNumbers = [5, 10, 8, 3];
+let count = 1;
+let multipliedNums = allNumbers.map((nums)=>{
+  count = count * nums;
+})
+
+console.log(count)
 
 // 8 Start with an array of strings and combine them all into a single string, separated by dashes.
 //  For example, ["volleyball", "basketball", "badminton"] becomes "-volleyball-basketball-badminton-".
+let allSports = ["volleyball", "basketball", "badminton"];
+let combinedSports = allSports.join("-");
+console.log(`-${combinedSports}-`);
+
 
 // 9 Start with an array of hashes and find the hash with the shortest name (from the :name key).
 //  For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "book", price: 4}.
+let myOffice = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}];
+let shortest = myOffice.sort((item, item2) => item.name.length - item2.name.length)[0];
+
+console.log(shortest)
+
 
 // 10 Start with an array of numbers and compute the maximum number.
 //  For example, [5, 10, 8, 3] becomes 10.
+
+let myNum = [5, 10, 8, 3];
+let maxNum = myNum.sort((a,b) => a - b)[myNum.length-1];
+console.log(maxNum);
